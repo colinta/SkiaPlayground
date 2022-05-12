cb45bd16ef89b7edaa9d70baf4afb5587066626b
Colin T.A. Gray <colinta@gmail.com>
2022-05-12 09:14:18 -0400

    all playgrounds – Circles, Mesh, Neumorphism

---
+++ README.md
@@ -0,0 +1,7 @@
+A bare bones repo to help get you started. This repo was created using:
+
+```
+npx react-native init SkiaSandbox --template react-native-template-typescript
+yarn add @shopify/react-native-skia
+yarn add simplex-noise
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React, {useState} from 'react';
-import {View} from 'react-native';
-import Playground from './Playground';
+import {View, Button} from 'react-native';
+import Circles from './Circles';
+import Mesh from './Mesh';
+import Neumorphism from './Neumorphism';
 
 type Size = {
   width: number;
   height: number;
 };
 
+type Show = 'Circles' | 'Mesh' | 'Neumorphism';
+
 export default function App() {
-  const [size, setSize] = useState<Size>({width: 0, height: 0});
+  const [size, setSize] = useState<Size>();
+  const [show, setShow] = useState<Show>();
 
   return (
-    <View
-      style={{flex: 1}}
-      onLayout={({
-        nativeEvent: {
-          layout: {width, height},
-        },
-      }) => setSize({width, height})}>
-      {Boolean(size) && <Playground size={size} />}
+    <View style={{flex: 1}}>
+      <View
+        style={{flex: 1}}
+        onLayout={({
+          nativeEvent: {
+            layout: {width, height},
+          },
+        }) => setSize({width, height})}>
+        {size && show === 'Circles' ? <Circles size={size} /> : null}
+        {size && show === 'Mesh' ? <Mesh size={size} /> : null}
+        {size && show === 'Neumorphism' ? <Neumorphism /> : null}
+        {size && !show ? <View style={{flex: 1}} /> : null}
+      </View>
+      <Button title="Circles" onPress={() => setShow('Circles')} />
+      <Button title="Mesh" onPress={() => setShow('Mesh')} />
+      <Button title="Neumorphism" onPress={() => setShow('Neumorphism')} />
+      <Button title="" onPress={() => {}} />
     </View>
   );
 }
